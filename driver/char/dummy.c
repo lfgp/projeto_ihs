e3cbc1637fb3a2ca1ceb23cf58b35cfc9c43b3d1
lfgp <48449255+lfgp@users.noreply.github.com>
2025-08-08 16:52:43 -0300

    Update dummy.c
    remove this module

--- driver/char/dummy.c
+++ driver/char/dummy.c
@@ -71,7 +71,7 @@ static int __init my_init(void)
 
 	/* 2. create class : appears at /sys/class (to create the device node/file managed by system) */
 
-	if ((my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
+	if ((my_class = class_create(DRIVER_CLASS)) == NULL) {
 		printk("my_driver: device class count not be created!\n");
 		goto ClassError;
 	}
